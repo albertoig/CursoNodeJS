56dff97b69e0d8d4f88ea2cd2085fca268b61c9f
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2015-11-03 18:13:33 +0100

    just more code to finish the exercises

--- exercises/exercise2/app.js
+++ exercises/exercise2/app.js
@@ -11,19 +11,29 @@ let url = argv.url;
 let convertArg = argv.convert;
 let leetArg = argv.leet;
 let sanitazeArg = argv.sanitaze;
-
+let countWordFrecuencyArg = argv.count;
 
 if (url !== undefined) {
     reader(url);
 }
 
 if (convertArg !== undefined) {
-    convert(url);
+    console.log(convert(convertArg));
 }
 
 if (leetArg !== undefined) {
-    leet(url);
+    console.log(leet(leetArg));
 }
+
+if (sanitazeArg !== undefined) {
+    console.log(sanitize(sanitazeArg,' '));
+}
+
+if (countWordFrecuencyArg !== undefined) {
+    console.log(countWordFrecuency(countWordFrecuencyArg));
+}
+
+
 
 
 
